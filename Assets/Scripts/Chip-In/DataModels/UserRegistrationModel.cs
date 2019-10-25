fd5a0fa89af8fb0e7ecdc1ab3fdea9a908b6e457
Roman_Shkirenko <dadlord36@gmail.com>
2019-10-25 18:12:35 +0300

    RegistrationViewModel created with registration data bindable and notifiable fields

--- Assets/Scripts/Chip-In/DataModels/UserRegistrationModel.cs
+++ Assets/Scripts/Chip-In/DataModels/UserRegistrationModel.cs
@@ -3,12 +3,44 @@ using System.Net.Mail;
 
 namespace DataModels
 {
-    [Serializable]
-    public class UserRegistrationModel
+    public interface IUserRegistrationModel
     {
-        public MailAddress email;
-        public string password;
-        public string gender;
-        public string role;
+        MailAddress Email { get; set; }
+        string Password { get; set; }
+        string Gender { get; set; }
+        string Role { get; set; }
+    }
+
+    [Serializable]
+    public class UserRegistrationModel : IUserRegistrationModel
+    {
+        private MailAddress email;
+        private string password;
+        private string gender;
+        private string role;
+
+        public MailAddress Email
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }
+
+        public string Gender
+        {
+            get => gender;
+            set => gender = value;
+        }
+
+        public string Role
+        {
+            get => role;
+            set => role = value;
+        }
     }
 }
---
+++ Assets/Scripts/Chip-In/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using DataModels;
+using JetBrains.Annotations;
+using UnityWeld.Binding;
+
+namespace ViewModels
+{
+    [Binding]
+    public class RegistrationViewModel : BaseViewModel, IUserRegistrationModel, INotifyPropertyChanged
+    {
+        private IUserRegistrationModel _registrationModel = new UserRegistrationModel();
+        [Binding]
+        public MailAddress Email
+        {
+            get => _registrationModel.Email;
+            set
+            {
+                _registrationModel.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+        [Binding]
+        public string Password
+        {
+            get => _registrationModel.Password;
+            set
+            {
+                _registrationModel.Password = value; 
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        [Binding]
+        public string Gender
+        {
+            get => _registrationModel.Gender;
+            set
+            {
+                _registrationModel.Gender = value;
+                OnPropertyChanged(nameof(Gender));
+            }
+        }
+        [Binding]
+        public string Role
+        {
+            get => _registrationModel.Role;
+            set
+            {
+                _registrationModel.Role = value;
+                OnPropertyChanged(nameof(Role));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
