0404fb2035780218ad4f7bd50ff21a0dfabf29e6
Roman_Shkirenko <dadlord36@gmail.com>
2019-10-28 15:04:52 +0300

    UI gradient effects moved to the corresponding namespaces

---
+++ Assets/Scripts/Chip-In/UI/Effects/UIGradient/SetDirty.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Effects.UIGradient
+{
+	public class SetDirty : MonoBehaviour {
+		public Graphic m_graphic;
+		// Use this for initialization
+		void Reset () {
+			m_graphic = GetComponent<Graphic>();
+		}
+	
+		// Update is called once per frame
+		void Update () {
+			m_graphic.SetVerticesDirty();
+		}
+	}
+}
