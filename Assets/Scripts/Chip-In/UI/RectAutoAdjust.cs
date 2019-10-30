ce77823720caf2c3d9860632bf4f083733b66558
Roman_Shkirenko <dadlord36@gmail.com>
2019-10-30 13:51:46 +0300

    UI Component, that allows to resize rect of UI element based on screen size (used to resize Container in ScrollView)

---
+++ Assets/Scripts/Chip-In/UI/RectAutoAdjust.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    [AddComponentMenu("UI/Adjusting/RectAutoAdjust")]
+    public class RectAutoAdjust : UIBehaviour
+    {
+        [SerializeField, Range(1, byte.MaxValue)]
+        private byte times = 1;
+
+        [SerializeField]
+        private Vector2Int targetResolution;
+
+        protected override void OnEnable()
+        {
+#if !UNITY_EDITOR
+            targetResolution = new Vector2Int(Screen.width,Screen.height
+#endif
+            base.OnEnable();
+            Adjust();
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            Adjust();
+        }
+#endif
+
+        private void Adjust()
+        {
+            if (!TryGetComponent(out RectTransform rectTransform)) return;
+            var sizeDelta = rectTransform.sizeDelta;
+            sizeDelta.x = targetResolution.x * times;
+            rectTransform.sizeDelta = sizeDelta;
+        }
+    }
+}
