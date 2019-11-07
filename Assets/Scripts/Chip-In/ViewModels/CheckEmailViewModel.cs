496bda005a6ce9d58ab3fa17736e72a4efee89c0
Roman_Shkirenko <dadlord36@gmail.com>
2019-11-07 10:50:34 +0300

    Check Email ViewModel implemened

---
+++ Assets/Scripts/Chip-In/ViewModels/CheckEmailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using UnityWeld.Binding;
+
+namespace ViewModels
+{
+    [Binding]
+    public class CheckEmailViewModel : BaseViewModel
+    {
+        [Binding]
+        public void SwitchToLoginView()
+        {
+            viewsSwitchingBinding.SwitchView<LoginViewModel>(View);
+        }
+    }
+}
