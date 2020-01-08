fd7057f9990c0195508069b23ff51d05e5a7cd6a
Roman_Shkirenko <dadlord36@gmail.com>
2020-01-08 12:12:27 +0300

    Circles generator added

---
+++ Assets/Scripts/Editor/Inspectors/CommunitySpiritAnalyticViewEditor.cs
@@ -0,0 +1,61 @@
+﻿using UI.Elements;
+using UnityEditor;
+using UnityEngine;
+
+namespace Inspectors
+{
+    [CustomEditor(typeof(CommunitySpiritAnalyticView))]
+    public class CommunitySpiritAnalyticViewEditor : Editor
+    {
+        private CommunitySpiritAnalyticView _analyticView;
+
+
+        private void OnEnable()
+        {
+            _analyticView = (CommunitySpiritAnalyticView) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            _analyticView.circlesThickness =
+                (uint) EditorGUILayout.IntField("Circle thickness", (int) _analyticView.circlesThickness);
+            _analyticView.arcSteps =
+                (uint) EditorGUILayout.IntField("Arc steps", (int) _analyticView.arcSteps);
+            _analyticView.circlesColor = EditorGUILayout.ColorField("Circles color", _analyticView.circlesColor);
+            _analyticView.scaleFactor = EditorGUILayout.Slider("Scale factor", _analyticView.scaleFactor, 0f, 1f);
+            _analyticView.circlesBaseSize = EditorGUILayout.IntField("Circles size", _analyticView.circlesBaseSize);
+
+            void UpdateCircles()
+            {
+                _analyticView.SetCirclesStyle(false, _analyticView.circlesBaseSize,_analyticView.arcSteps, _analyticView.circlesThickness,
+                    _analyticView.circlesColor);
+                _analyticView.ScaleCircles(_analyticView.scaleFactor);
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                UpdateCircles();
+            }
+
+            if (GUILayout.Button("Add circle"))
+            {
+                _analyticView.AddCircle();
+                UpdateCircles();
+            }
+
+            if (GUILayout.Button("Delete circle"))
+            {
+                _analyticView.RemoveLastCircle();
+            }
+
+            if (GUILayout.Button("Remove all circles"))
+            {
+                _analyticView.ClearCirclesArray();
+            }
+
+            base.OnInspectorGUI();
+        }
+    }
+}
