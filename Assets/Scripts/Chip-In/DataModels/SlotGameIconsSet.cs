460682260291dcf5f88b709fe1c812d4862d6df8
Roman_Shkirenko <dadlord36@gmail.com>
2020-02-05 14:47:22 +0300

    Basics of SlotIconsRepository implemented
    It is left to implement loading data from server logic

---
+++ Assets/Scripts/Chip-In/DataModels/SlotGameIconsSet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DataModels.Interfaces;
+using UnityEngine;
+
+namespace DataModels
+{
+    [Serializable]
+    public struct SlotGameIconsSet : ISlotGameIconsSet
+    {
+        [SerializeField] private Sprite first;
+        [SerializeField] private Sprite second;
+        [SerializeField] private Sprite third;
+        [SerializeField] private Sprite fourth;
+
+        public Sprite First
+        {
+            get => first;
+            set => first = value;
+        }
+
+        public Sprite Second
+        {
+            get => second;
+            set => second = value;
+        }
+
+        public Sprite Third
+        {
+            get => third;
+            set => third = value;
+        }
+
+        public Sprite Fourth
+        {
+            get => fourth;
+            set => fourth = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Chip-In/ScriptableObjects/DataSets/SlotGameIconsSetScriptableObject.cs
@@ -0,0 +1,36 @@
+﻿using DataModels;
+using DataModels.Interfaces;
+using UnityEngine;
+
+namespace ScriptableObjects.DataSets
+{
+    [CreateAssetMenu(fileName = nameof(SlotGameIconsSetScriptableObject), menuName = nameof(DataSets) + "/" + nameof(SlotGameIconsSetScriptableObject),
+        order = 0)]
+    public class SlotGameIconsSetScriptableObject : ScriptableObject, ISlotGameIconsSet
+    {
+        [SerializeField] private SlotGameIconsSet slotGameIconsSet;
+        public Sprite First
+        {
+            get => slotGameIconsSet.First;
+            set => slotGameIconsSet.First = value;
+        }
+
+        public Sprite Second
+        {
+            get => slotGameIconsSet.Second;
+            set => slotGameIconsSet.Second = value;
+        }
+
+        public Sprite Third
+        {
+            get => slotGameIconsSet.Third;
+            set => slotGameIconsSet.Third = value;
+        }
+
+        public Sprite Fourth
+        {
+            get => slotGameIconsSet.Fourth;
+            set => slotGameIconsSet.Fourth = value;
+        }
+    }
+}
