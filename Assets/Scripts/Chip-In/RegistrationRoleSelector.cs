e81b82852b7a43f8db5c8359d1f51f9d889d9570
Roman_Shkirenko <dadlord36@gmail.com>
2020-02-17 15:57:01 +0300

    User registration default role selection fixed

--- Assets/Scripts/Chip-In/RegistrationRoleSelector.cs
+++ Assets/Scripts/Chip-In/RegistrationRoleSelector.cs
@@ -43,6 +43,7 @@ public sealed class RegistrationRoleSelector : MonoBehaviour
     private void OnEnable()
     {
         selectedByDefaultButton.PerformGroupActionWithoutNotification();
+        SelectUserRole();
     }
 
     private void OnRoleChanged(string obj)
