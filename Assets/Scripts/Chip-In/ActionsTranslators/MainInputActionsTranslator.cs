03a97113fa60fc13c71afe069a3d0c2e85ba9847
Roman_Shkirenko <dadlord36@gmail.com>
2020-02-27 20:59:50 +0300

    MainInputActionsTranslator scriptable object, that is translating input action events is implemented.

---
+++ Assets/Scripts/Chip-In/ActionsTranslators/MainInputActionsTranslator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace ActionsTranslators
+{
+    public interface IUpdatable
+    {
+        void Update();
+    }
+    
+    [CreateAssetMenu(fileName = nameof(MainInputActionsTranslator), menuName = nameof(ActionsTranslators) + "/" + nameof(MainInputActionsTranslator),
+        order = 0)]
+    public sealed class MainInputActionsTranslator : ScriptableObject, IUpdatable
+    {
+        public event Action EscapeButtonPressed;
+        void IUpdatable.Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                OnReturnButtonPressed();
+            }
+        }
+
+        private void OnReturnButtonPressed()
+        {
+            EscapeButtonPressed?.Invoke();
+        }
+    }
+}
