a0949f1b5395386973808ea2d3f7c50a97c071ef
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-06 19:01:18 +0300

    LoginForm input-fields auto switching implemented

--- Assets/Scripts/Chip-In/Views/LoginView.cs
+++ Assets/Scripts/Chip-In/Views/LoginView.cs
@@ -1,5 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Views
 {
@@ -8,29 +10,93 @@ namespace Views
         [SerializeField] private TMP_InputField emailField;
         [SerializeField] private TMP_InputField passwordField;
 
+        private TouchScreenKeyboard _keyboard;
+        private bool _emailFieldIsSelected;
+
+        private TMP_InputField _selectedInputField;
+
         protected override void OnEnable()
         {
             base.OnEnable();
             ClearFields();
+            emailField.onSelect.AddListener(OpenEmailKeyboard);
+            passwordField.onSelect.AddListener(OpenPasswordKeyboard);
         }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            emailField.onSelect.RemoveListener(OpenEmailKeyboard);
+            passwordField.onSelect.RemoveListener(OpenPasswordKeyboard);
+        }
+
+        private void OpenEmailKeyboard(string text)
+        {
+            _keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.EmailAddress);
+            _emailFieldIsSelected = true;
+            _selectedInputField = emailField;
+        }
+
+        private void OpenPasswordKeyboard(string text)
+        {
+            _keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
+            _selectedInputField = passwordField;
+        }
+
 
         private void Update()
         {
+#if UNITY_EDITOR
             if (Input.GetKeyUp(KeyCode.Tab))
             {
                 SwitchSelectedInputField();
             }
+
+#elif UNITY_ANDROID || UNITY_IOS
+            ProcessMobileKeyboard();
+#endif
+        }
+
+        private void ProcessMobileKeyboard()
+        {
+            if (_keyboard == null) return;
+
+            _selectedInputField.text = _keyboard.text;
+
+
+            if (_keyboard.status != TouchScreenKeyboard.Status.Done) return;
+
+            _keyboard = null;
+
+            if (!_emailFieldIsSelected) return;
+            Invoke(nameof(SwitchToPasswordInput), 0.1f);
+        }
+
+        private void SwitchToPasswordInput()
+        {
+            _emailFieldIsSelected = false;
+            SetInputFieldActive(passwordField);
         }
 
         private void SwitchSelectedInputField()
         {
             if (emailField.isFocused)
             {
-                passwordField.Select();
+                SetInputFieldActive(passwordField);
                 return;
             }
-            emailField.Select();
+
+            SetInputFieldActive(emailField);
         }
+
+        private static void SetInputFieldActive(TMP_InputField inputField)
+        {
+            var system = EventSystem.current;
+
+            inputField.OnPointerClick(new PointerEventData(system));
+            system.SetSelectedGameObject(inputField.gameObject, new BaseEventData(system));
+        }
+
 
         private void ClearFields()
         {
