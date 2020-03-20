e4ce010544101c08a5905c1a1090b6e2e902378d
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-20 12:57:24 +0300

    OmniSART FPSCounter added from Assets Store.

---
+++ Assets/Scripts/Chip-In/FPSLimit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FpsLimit : MonoBehaviour
+{
+    [SerializeField] private int maxFPS=60;
+    public void ChangeFrameRate()
+    {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = maxFPS;
+    }
+
+    private void Start()
+    {
+        ChangeFrameRate();
+    }
+}
