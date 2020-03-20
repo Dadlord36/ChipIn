c2308fe7f2c531bfd292de63fa3ccc36be924d22
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-20 18:26:17 +0300

    Admin form with view and view model are added

---
+++ Assets/Scripts/Chip-In/ViewModels/AdminViewModel.cs
@@ -0,0 +1,61 @@
+﻿using UnityWeld.Binding;
+using Views.Cards;
+
+namespace ViewModels
+{
+    [Binding]
+    public class AdminViewModel : ViewsSwitchingViewModel, IAdminProperties
+    {
+        private IAdminProperties AdminViewProperties => View as IAdminProperties;
+
+        public uint TokenBallance
+        {
+            get => AdminViewProperties.TokenBallance;
+            set => AdminViewProperties.TokenBallance = value;
+        }
+
+        public uint AdSpendField
+        {
+            get => AdminViewProperties.AdSpendField;
+            set => AdminViewProperties.AdSpendField = value;
+        }
+
+        public uint SalesFromThisApp
+        {
+            get => AdminViewProperties.SalesFromThisApp;
+            set => AdminViewProperties.SalesFromThisApp = value;
+        }
+
+        public uint SalesCommisions
+        {
+            get => AdminViewProperties.SalesCommisions;
+            set => AdminViewProperties.SalesCommisions = value;
+        }
+
+        public uint ReturnOnInvestments
+        {
+            get => AdminViewProperties.ReturnOnInvestments;
+            set => AdminViewProperties.ReturnOnInvestments = value;
+        }
+
+        [Binding]
+        public void TokenBallance_OnClick()
+        {
+        }
+
+        [Binding]
+        public void Access_OnClick()
+        {
+        }
+
+        [Binding]
+        public void Verification_OnClick()
+        {
+        }
+
+        [Binding]
+        public void Library_OnClick()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Chip-In/Views/Cards/AdminView.cs
@@ -0,0 +1,54 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Views.Cards
+{
+    public interface IAdminProperties
+    {
+        uint TokenBallance { get; set; }
+        uint AdSpendField { get; set; }
+        uint SalesFromThisApp { get; set; }
+        uint SalesCommisions { get; set; }
+        uint ReturnOnInvestments { get; set; }
+    }
+
+    public class AdminView : BaseView, IAdminProperties
+    {
+        [SerializeField] private TMP_Text tokenBallanceTextField;
+
+        [SerializeField] private TMP_Text adSpendField;
+        [SerializeField] private TMP_Text salesFromThisAppTextField;
+        [SerializeField] private TMP_Text salesCommisionsTextField;
+        [SerializeField] private TMP_Text returnOnInvestmentsTextField;
+
+        public uint TokenBallance
+        {
+            get => uint.Parse(tokenBallanceTextField.text);
+            set => tokenBallanceTextField.text = value.ToString();
+        }
+
+        public uint AdSpendField
+        {
+            get => uint.Parse(adSpendField.text);
+            set => adSpendField.text = value.ToString();
+        }
+
+        public uint SalesFromThisApp
+        {
+            get => uint.Parse(salesFromThisAppTextField.text);
+            set => salesFromThisAppTextField.text = value.ToString();
+        }
+
+        public uint SalesCommisions
+        {
+            get => uint.Parse(salesCommisionsTextField.text);
+            set => salesCommisionsTextField.text = value.ToString();
+        }
+
+        public uint ReturnOnInvestments
+        {
+            get => uint.Parse(returnOnInvestmentsTextField.text);
+            set => returnOnInvestmentsTextField.text = value.ToString();
+        }
+    }
+}
