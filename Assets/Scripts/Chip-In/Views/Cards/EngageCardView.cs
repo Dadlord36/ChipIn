6e22da6d93ecbabb35f0320bbf565e604840ade3
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-20 18:28:11 +0300

    Engage card and Engage form are implemented.

---
+++ Assets/Scripts/Chip-In/Views/Cards/EngageCardView.cs
@@ -0,0 +1,54 @@
+﻿using TMPro;
+using UnityEngine;
+using ViewModels.UI.Elements.Icons;
+
+namespace Views.Cards
+{
+    public class EngageCardView : BaseView
+    {
+        [SerializeField] private TMP_Text titleTextField;
+        [SerializeField] private TMP_Text descriptionAgeTextField;
+        [SerializeField] private TMP_Text marketAgeTextField;
+        [SerializeField] private TMP_Text marketSizeTextField;
+        [SerializeField] private TMP_Text marketCapTextField;
+        [SerializeField] private TMP_Text marketSpiritTextField;
+        [SerializeField] private UserAvatarIcon icon;
+
+
+        public string Title
+        {
+            get => titleTextField.text;
+            set => descriptionAgeTextField.text = value;
+        }
+        
+        public Sprite IconSprite
+        {
+            get => icon.AvatarSprite;
+            set => icon.AvatarSprite = value;
+        }
+
+        public string MarketAge
+        {
+            get => marketAgeTextField.text;
+            set => marketAgeTextField.text = value;
+        }
+
+        public string MarketSize
+        {
+            get => marketSizeTextField.text;
+            set => marketSizeTextField.text = value;
+        }
+
+        public string MarketCap
+        {
+            get => marketCapTextField.text;
+            set => marketCapTextField.text = value;
+        }
+
+        public string MarketSpirit
+        {
+            get => marketSpiritTextField.text;
+            set => marketSpiritTextField.text = value;
+        }
+    }
+}
