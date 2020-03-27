496abb68049c4790cab81f496ccc7f7ea81fc5d1
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-27 19:44:27 +0300

    Free barcode plugin implementation.

--- Assets/Scripts/Chip-In/ViewModels/QrCodeScannerViewModel.cs
+++ Assets/Scripts/Chip-In/ViewModels/QrCodeScannerViewModel.cs
@@ -12,39 +12,6 @@ namespace ViewModels
     {
         [SerializeField] private RawImage cameraViewImage;
         [SerializeField] private AudioSource audioSource;
-        [SerializeField] private TMP_Text textHeader;
-        [SerializeField] private AspectRatioFitter fitter;
-
-        [SerializeField] private TMP_InputField rotationXText;
-        [SerializeField] private TMP_InputField rotationYText;
-        [SerializeField] private TMP_InputField rotationZText;
-
-        [SerializeField] private TMP_InputField scaleXText;
-        [SerializeField] private TMP_InputField scaleYText;
-        [SerializeField] private TMP_InputField scaleZText;
-
-        private float rotationX => float.Parse(rotationXText.text);
-        private float rotationY => float.Parse(rotationYText.text);
-        private float rotationZ => float.Parse(rotationZText.text);
-
-        private float scaleX
-        {
-            get { return float.Parse(scaleXText.text); }
-            set { scaleXText.text = value.ToString(); }
-        }
-
-        private float scaleY
-        {
-            get { return float.Parse(scaleYText.text); }
-            set { scaleYText.text = value.ToString(); }
-        }
-
-        private float scaleZ
-        {
-            get { return float.Parse(scaleZText.text); }
-            set { scaleZText.text = value.ToString(); }
-        }
-
 
         private IScanner _barcodeScanner;
         private float _restartTime;
@@ -77,10 +44,13 @@ namespace ViewModels
 
             if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
             {
-                StartCoroutine(AuthorizeWebCamera());
+                AuthorizeWebCamera();
+            }
+            else
+            {
+                _barcodeScanner.Camera.Play();
             }
 
-            _barcodeScanner.Camera.Play();
             // Display the camera texture through a RawImage
             _barcodeScanner.OnReady += ProjectCameraOnScreen;
         }
@@ -90,37 +60,37 @@ namespace ViewModels
             _barcodeScanner.Camera.Stop();
         }
 
-        IEnumerator AuthorizeWebCamera()
+        void AuthorizeWebCamera()
         {
             // When the app start, ask for the authorization to use the webcam
-            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-
-            if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+            Application.RequestUserAuthorization(UserAuthorization.WebCam).completed += delegate
             {
-                throw new Exception("This Webcam library can't work without the webcam authorization");
-            }
+                if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+                {
+                    throw new Exception("This Webcam library can't work without the webcam authorization");
+                }
+
+                _barcodeScanner.Camera.Play();
+            };
         }
 
 
         private void ProjectCameraOnScreen(object sender, EventArgs eventArgs)
         {
-            //ToDo Make camera image fit the screen
             cameraViewImage.texture = _barcodeScanner.Camera.Texture;
 
             var imageTransform = cameraViewImage.transform;
             imageTransform.localEulerAngles = _barcodeScanner.Camera.GetEulerAngles();
 
-            var scale = imageTransform.localScale = _barcodeScanner.Camera.GetScale();
-            scale.y = (float)  _barcodeScanner.Camera.Height / _barcodeScanner.Camera.Width;
-            imageTransform.localScale = scale;
-
-            scaleX = scale.x;
-            scaleY = scale.y;
-            scaleZ = scale.z;
-
-            // fitter.aspectRatio = (float) _barcodeScanner.Camera.Width / _barcodeScanner.Camera.Height;
-            ((RectTransform) imageTransform).sizeDelta = new Vector2(Screen.width, Screen.height);
-            textHeader.text = $"Width: {_barcodeScanner.Camera.Width.ToString()}; Height: {_barcodeScanner.Camera.Height.ToString()}";
+            if (Screen.width != _barcodeScanner.Camera.Width && Screen.height != _barcodeScanner.Camera.Height)
+            {
+                var aspectRation = (float) _barcodeScanner.Camera.Height / _barcodeScanner.Camera.Width;
+                imageTransform.localScale = new Vector3(aspectRation, aspectRation, aspectRation);
+            }
+            else
+            {
+                imageTransform.localScale = _barcodeScanner.Camera.GetScale();
+            }
 
             _restartTime = Time.realtimeSinceStartup;
         }
@@ -133,12 +103,6 @@ namespace ViewModels
             _barcodeScanner.Scan((barCodeType, barCodeValue) =>
             {
                 _barcodeScanner.Stop();
-                if (textHeader.text.Length > 250)
-                {
-                    textHeader.text = "";
-                }
-
-                textHeader.text += "Found: " + barCodeType + " / " + barCodeValue + "\n";
                 _restartTime += Time.realtimeSinceStartup + 1f;
 
                 // Feedback
@@ -157,9 +121,6 @@ namespace ViewModels
         void Update()
         {
             if (_barcodeScanner == null) return;
-
-            // ((RectTransform) cameraViewImage.transform).eulerAngles = new Vector3(rotationX, rotationY, rotationZ);
-            ((RectTransform) cameraViewImage.transform).localScale = new Vector3(scaleX, scaleY, scaleZ);
             _barcodeScanner.Update();
 
             // Check if the Scanner need to be started or restarted
