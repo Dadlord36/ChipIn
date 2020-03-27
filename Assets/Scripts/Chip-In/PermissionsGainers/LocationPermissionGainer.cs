3f23024698e665780f62f1e0243970e724c2dea5
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-27 19:28:53 +0300

    LocationPermissionGainer added.

---
+++ Assets/Scripts/Chip-In/PermissionsGainers/LocationPermissionGainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Android;
+
+namespace PermissionsGainers
+{
+    public sealed class LocationPermissionGainer : MonoBehaviour
+    {
+        public event Action RequestAccepted;
+        public event Action RequestRejected;
+
+        public void Request()
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            if (Input.location.isEnabledByUser)
+            {
+                OnRequestAccepted();
+            }
+            else
+            {
+                OnRequestRejected();
+            }
+            Destroy(gameObject);
+        }
+
+        private void OnRequestAccepted()
+        {
+            RequestAccepted?.Invoke();
+        }
+
+        private void OnRequestRejected()
+        {
+            RequestRejected?.Invoke();
+        }
+    }
+}
