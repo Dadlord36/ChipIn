9767f112a5badb1892eb0b555f405beae1c8e104
Roman_Shkirenko <dadlord36@gmail.com>
2020-03-27 18:47:55 +0300

    Implementing apps mode switching and merchant bottom bar.

---
+++ Assets/Scripts/Chip-In/Controllers/AppModesSwitcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Controllers
+{
+    public class AppModesSwitcher : MonoBehaviour
+    {
+        [SerializeField] private SessionController sessionController;
+
+        [SerializeField] private Transform bottomBarContainer;
+
+        [Space(10)] [SerializeField] private Object userAppBottomBarPrefab;
+        [SerializeField] private Object merchantAppBottomBarPrefab;
+
+        private void Start()
+        {
+            sessionController.SwitchingToMode += SessionControllerOnSwitchingToMode;
+        }
+
+        private void SessionControllerOnSwitchingToMode(SessionController.SessionMode mode)
+        {
+            switch (mode)
+            {
+                case SessionController.SessionMode.User:
+                    SwitchToUserMode();
+                    break;
+                case SessionController.SessionMode.Merchant:
+                    SwitchToMerchantMode();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+        }
+
+        private void SwitchToUserMode()
+        {
+            ClearContainer();
+            Instantiate(userAppBottomBarPrefab, bottomBarContainer);
+        }
+
+        private void SwitchToMerchantMode()
+        {
+            ClearContainer();
+            Instantiate(merchantAppBottomBarPrefab, bottomBarContainer);
+        }
+
+        private void ClearContainer()
+        {
+            if (bottomBarContainer.childCount > 0)
+                Destroy(bottomBarContainer.GetChild(0).gameObject);
+        }
+    }
+}
