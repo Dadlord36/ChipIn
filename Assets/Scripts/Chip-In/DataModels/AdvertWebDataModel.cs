fb07ff17c0d94d66d7c7c7d7b33a6308115b4b23
Roman_Shkirenko <dadlord36@gmail.com>
2020-04-27 16:26:03 +0300

    Engage menu button blocked.

---
+++ Assets/Scripts/Chip-In/DataModels/AdvertWebDataModel.cs
@@ -0,0 +1,14 @@
+﻿using DataModels.Interfaces;
+using DataModels.SimpleTypes;
+using Views;
+
+namespace DataModels
+{
+    public class AdvertWebDataModel : IAdvertWebModel
+    {
+        public int? Id { get; set; }
+        public string Slogan { get; set; }
+        public FilePath PosterFilePath { get; set; }
+        public AdvertFeatureDataModel[] Features { get; set; }
+    }
+}
