54967c39c202fa16fa50b86fa27dca66dfb463f0
Roman_Shkirenko <dadlord36@gmail.com>
2020-06-05 19:06:15 +0300

    Implementing UpdateTranslationBehaviour.cs.

---
+++ Assets/Scripts/Chip-In/Behaviours/Common/UpdateTranslationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using ActionsTranslators;
+using UnityEngine;
+
+namespace Behaviours.Common
+{
+    public class UpdateTranslationBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Object[] objectsWithUpdatableInterface;
+        private IUpdatable[] _updatableInterfaces;
+
+        private void Start()
+        {
+            _updatableInterfaces = new IUpdatable[objectsWithUpdatableInterface.Length];
+            for (int i = 0; i < objectsWithUpdatableInterface.Length; i++)
+            {
+                _updatableInterfaces[i] = objectsWithUpdatableInterface[i] as IUpdatable;
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _updatableInterfaces.Length; i++)
+            {
+                _updatableInterfaces[i].Update();
+            }
+        }
+    }
+}
