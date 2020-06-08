95689b3401626b5455e4cd465aa3adabb0cb7d31
Roman_Shkirenko <dadlord36@gmail.com>
2020-06-08 13:34:19 +0300

    Refactoring LogUtility.cs

--- Assets/Scripts/Chip-In/Utilities/LogUtility.cs
+++ Assets/Scripts/Chip-In/Utilities/LogUtility.cs
@@ -6,33 +6,30 @@ namespace Utilities
 {
     public static class LogUtility
     {
- 
-        
         private const bool UseLog = false;
 
-        public static void PrintLog(in string tag, in string message, Object context=null)
+        public static void PrintLog(in string tag, in string message, Object context = null)
         {
 #if UseLOG
-             Debug.unityLogger.Log(tag,message, context);
-#endif
-           
-        }        
-        
-        public static void PrintLogWarning(in string tag, in string message,Object context=null)
-        {
-#if UseLOG
-            Debug.unityLogger.LogWarning(tag,message,context);
+            Debug.unityLogger.Log(tag, message, context);
 #endif
         }
-        
-        public static void PrintLogError(in string tag, in string message,Object context=null)
+
+        public static void PrintLogWarning(in string tag, in string message, Object context = null)
         {
 #if UseLOG
-            Debug.unityLogger.LogError(tag,message,context);
+            Debug.unityLogger.LogWarning(tag, message, context);
 #endif
         }
-        
-        public static void PrintLogException(Exception exception,Object context=null)
+
+        public static void PrintLogError(in string tag, in string message, Object context = null)
+        {
+#if UseLOG
+            Debug.unityLogger.LogError(tag, message, context);
+#endif
+        }
+
+        public static void PrintLogException(Exception exception, Object context = null)
         {
 #if UseLOG
             Debug.unityLogger.LogException(exception, context);
