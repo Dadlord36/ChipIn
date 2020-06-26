251000ad3d92ec3aa19f1ce57735e7412226c264
Roman <Dadlord36@gmail.com>
2020-06-26 18:11:09 +0300

    LoginForm input fields functionality fixed.

--- Assets/Scripts/Chip-In/Views/LoginView.cs
+++ Assets/Scripts/Chip-In/Views/LoginView.cs
@@ -1,7 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Views
 {
@@ -9,99 +8,28 @@ namespace Views
     {
         [SerializeField] private TMP_InputField emailField;
         [SerializeField] private TMP_InputField passwordField;
-
-        private TouchScreenKeyboard _keyboard;
-        private bool _emailFieldIsSelected;
-
-        private TMP_InputField _selectedInputField;
-
+        
         public LoginView() : base(nameof(LoginView))
         {
         }
 
-        protected override void OnEnable()
+
+        protected override void Start()
         {
-            base.OnEnable();
+            base.Start();
+            emailField.onSubmit.AddListener(delegate
+            {
+                ClearInputField(passwordField);
+                passwordField.OnSelect(new PointerEventData(EventSystem.current));
+            });
+        }
+
+        protected override void OnBeingSwitchedTo()
+        {
+            base.OnBeingSwitchedTo();
             ClearFields();
-            emailField.onSelect.AddListener(OpenEmailKeyboard);
-            passwordField.onSelect.AddListener(OpenPasswordKeyboard);
         }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-            emailField.onSelect.RemoveListener(OpenEmailKeyboard);
-            passwordField.onSelect.RemoveListener(OpenPasswordKeyboard);
-        }
-
-        private void OpenEmailKeyboard(string text)
-        {
-            _keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.EmailAddress);
-            _emailFieldIsSelected = true;
-            _selectedInputField = emailField;
-        }
-
-        private void OpenPasswordKeyboard(string text)
-        {
-            _keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
-            _selectedInputField = passwordField;
-        }
-
-
-        private void Update()
-        {
-#if UNITY_EDITOR
-            if (Input.GetKeyUp(KeyCode.Tab))
-            {
-                SwitchSelectedInputField();
-            }
-
-#elif UNITY_ANDROID || UNITY_IOS
-            ProcessMobileKeyboard();
-#endif
-        }
-
-        private void ProcessMobileKeyboard()
-        {
-            if (_keyboard == null) return;
-
-            _selectedInputField.text = _keyboard.text;
-
-
-            if (_keyboard.status != TouchScreenKeyboard.Status.Done) return;
-
-            _keyboard = null;
-
-            if (!_emailFieldIsSelected) return;
-            Invoke(nameof(SwitchToPasswordInput), 0.1f);
-        }
-
-        private void SwitchToPasswordInput()
-        {
-            _emailFieldIsSelected = false;
-            SetInputFieldActive(passwordField);
-        }
-
-        private void SwitchSelectedInputField()
-        {
-            if (emailField.isFocused)
-            {
-                SetInputFieldActive(passwordField);
-                return;
-            }
-
-            SetInputFieldActive(emailField);
-        }
-
-        private static void SetInputFieldActive(TMP_InputField inputField)
-        {
-            var system = EventSystem.current;
-
-            inputField.OnPointerClick(new PointerEventData(system));
-            system.SetSelectedGameObject(inputField.gameObject, new BaseEventData(system));
-        }
-
-
+        
         private void ClearFields()
         {
             ClearInputField(emailField);
