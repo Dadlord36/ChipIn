3cbef7fbcc008fc80cc34f41c011d45c786d4937
Roman <Dadlord36@gmail.com>
2020-06-30 20:36:19 +0300

    FloatProgressionTwinner created.

---
+++ Assets/Scripts/Chip-In/Common/Structures/ProgressionTwiners/FloatProgressionTwiner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Common.Structures.ProgressionTwiners
+{
+    public struct FloatProgressionTwiner
+    {
+        private float _initial, _target, _current;
+        public float CurrentValue => _current;
+
+        public FloatProgressionTwiner(float initial, float target) : this()
+        {
+            _initial = initial;
+            _target = target;
+        }
+
+        public float PreProgress(in float difference)
+        {
+            _current += difference;
+            return Mathf.InverseLerp(_initial, _target, _current);
+        }
+    }
+}
