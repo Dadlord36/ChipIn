ad6fbdb0e9091c4501f76fe8b37bcaea1d3d9e82
Roman <Dadlord36@gmail.com>
2020-07-09 17:23:27 +0300

    ThankYou form updated to the new design.

---
+++ Assets/Scripts/Chip-In/Views/InteractiveWindows/ThankYouView.cs
@@ -0,0 +1,32 @@
+﻿using Shapes2D;
+using TMPro;
+using UnityEngine;
+using UnityWeld.Binding;
+
+namespace Views.InteractiveWindows
+{
+    [Binding]
+    public class ThankYouView : BaseView
+    {
+        [SerializeField] private TMP_Text productNameTextField;
+        [SerializeField] private Shape productIconShape;
+
+        [Binding]
+        public string ProductName
+        {
+            get => productNameTextField.text;
+            set => productNameTextField.text = value;
+        }
+
+        [Binding]
+        public Texture2D ProductIcon
+        {
+            get => productIconShape.settings.fillTexture;
+            set => productIconShape.settings.fillTexture = value;
+        }
+
+        public ThankYouView() : base(nameof(ThankYouView))
+        {
+        }
+    }
+}
