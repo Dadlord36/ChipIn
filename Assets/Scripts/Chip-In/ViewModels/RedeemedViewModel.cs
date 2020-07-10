19dcce979621cf96f5f0ced45216c6d58dded167
Roman <Dadlord36@gmail.com>
2020-07-10 21:05:45 +0300

    Redemption and Redeemed form updated.

---
+++ Assets/Scripts/Chip-In/ViewModels/RedeemedViewModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityWeld.Binding;
+using Views;
+
+namespace ViewModels
+{
+    [Binding]
+    public class RedeemedViewModel : ViewsSwitchingViewModel
+    {
+        public RedeemedViewModel() : base(nameof(RedeemedViewModel))
+        {
+        }
+
+        [Binding]
+        public void CancelButton_OnClick()
+        {
+            SwitchToView(nameof(TransactView));
+        }
+    }
+}
