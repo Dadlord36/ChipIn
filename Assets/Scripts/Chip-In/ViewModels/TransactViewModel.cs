12669cded0203d49fb5d836d28af0f68adbae3bd
Roman <Dadlord36@gmail.com>
2020-07-10 18:00:00 +0300

    Transact form updated to the new design (1st iteration).

--- Assets/Scripts/Chip-In/ViewModels/TransactViewModel.cs
+++ Assets/Scripts/Chip-In/ViewModels/TransactViewModel.cs
@@ -1,14 +1,24 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Repositories.Remote;
+using RequestsStaticProcessors;
+using UnityEngine;
 using UnityWeld.Binding;
+using Utilities;
 using Views;
+using Views.ViewElements;
 
 namespace ViewModels
 {
     [Binding]
     public sealed class TransactViewModel : ViewsSwitchingViewModel, INotifyPropertyChanged
     {
+        [SerializeField] private UserAuthorisationDataRepository authorisationDataRepository;
+        [SerializeField] private DotsDiagram dotsDiagram;
+
         private bool _alternativeDiagram;
 
         [Binding]
@@ -27,6 +37,21 @@ namespace ViewModels
         {
         }
 
+        protected override async void OnBecomingActiveView()
+        {
+            base.OnBecomingActiveView();
+            try
+            {
+                await TryUpdateRadarViewData();
+            }
+            catch (Exception e)
+            {
+               LogUtility.PrintLogException(e);
+                throw;
+            }
+        }
+
+
         [Binding]
         public void Redemption_OnClick()
         {
@@ -42,6 +67,31 @@ namespace ViewModels
         public void SwitchDiagram()
         {
             AlternativeDiagram = !AlternativeDiagram;
+        }
+
+
+        private async Task TryUpdateRadarViewData()
+        {
+            try
+            {
+                var response = await MerchantMarketRequestsStaticProcessor.GetRadarData(out OperationCancellationController.TasksCancellationTokenSource,
+                    authorisationDataRepository);
+                if (!response.Success) return;
+
+                var responseModel = response.ResponseModelInterface;
+                if (responseModel.Data.Points == null)
+                {
+                    LogUtility.PrintLog(Tag, "There are no points were returned");
+                    return;
+                }
+
+                dotsDiagram.SetDataToVisualize(responseModel.Data);
+            }
+            catch (Exception e)
+            {
+                LogUtility.PrintLogException(e);
+                throw;
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ Assets/Scripts/Chip-In/Views/ViewElements/DotsDiagram.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using HttpRequests.RequestsProcessors.GetRequests;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.Extensions;
+
+namespace Views.ViewElements
+{
+    [RequireComponent(typeof(UICircle))]
+    public sealed class DotsDiagram : UIBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField] private UILineRenderer axisLineRenderer;
+        [SerializeField] private Radar radar;
+        [SerializeField] private Object dotViewPrefab;
+        
+        [SerializeField] private Vector2 firstColumnAngles, secondColumnAngles, thirdColumnAngles;
+        #endregion
+
+        private List<GameObject> _dotsViews = new List<GameObject>();
+
+        private IRadar Radar => radar;
+        private UICircle LargestCircle => Radar.LargestCircle;
+
+
+/*#if UNITY_EDITOR
+        [SerializeField] private DotInCircle[] dots;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].CalculatePosition(LargestCircle);
+                Gizmos.DrawSphere(dots[i].DotPosition, 10f);
+            }
+        }
+#endif*/
+
+        public void SetAxis()
+        {
+            var axisArray = GetDiagramAxisEndPoints();
+            var segmentsFromCenter = new List<Vector2>(axisArray.Length * 2);
+
+            var center = LargestCircle.transform.localPosition;
+
+            for (int i = 0; i < axisArray.Length; i++)
+            {
+                segmentsFromCenter.Add(center);
+                segmentsFromCenter.Add(axisArray[i]);
+            }
+
+            axisLineRenderer.Points = segmentsFromCenter.ToArray();
+        }
+        
+        private Vector2[] GetDiagramAxisEndPoints()
+        {
+            var diagramAxisAngles = new[]
+            {
+                firstColumnAngles.x, secondColumnAngles.x, thirdColumnAngles.x,
+                thirdColumnAngles.y, secondColumnAngles.y, firstColumnAngles.y
+            };
+
+            var endPoints = new Vector2[diagramAxisAngles.Length];
+
+            for (int i = 0; i < diagramAxisAngles.Length; i++)
+            {
+                var endPoint = new DotInCircle().CalculatePosition(LargestCircle,
+                    diagramAxisAngles[i], 1f);
+                endPoints[i] = transform.InverseTransformPoint(endPoint);
+            }
+
+            return endPoints;
+        }
+
+
+        public void SetDataToVisualize(RadarData radarData)
+        {
+            ClearDotsViews();
+
+            var points = radarData.Points;
+            var pointsCount = points.GetLength(0);
+            _dotsViews = new List<GameObject>(pointsCount);
+
+            var positions = Radar.CalculatePositionsForGivenRadarPoints(points, radarData.Max);
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                CreateDotAtPosition(positions[i]);
+            }
+        }
+
+        private GameObject CreateDotAtPosition(Vector2 position)
+        {
+            var gO = (GameObject) Instantiate(dotViewPrefab, transform);
+            gO.transform.position = position;
+            _dotsViews.Add(gO);
+            return gO;
+        }
+
+        private void ClearDotsViews()
+        {
+            if (_dotsViews == null) return;
+
+            for (int i = 0; i < _dotsViews.Count; i++)
+            {
+                DoDestroy(_dotsViews[i]);
+            }
+        }
+
+        private void DoDestroy(Object objectToDestroy)
+        {
+#if UNITY_EDITOR
+            DestroyImmediate(objectToDestroy);
+#else
+    Destroy(objectToDestroy);
+#endif
+        }
+    }
+}
