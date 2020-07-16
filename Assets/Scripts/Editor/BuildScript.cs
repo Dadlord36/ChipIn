01ecdfcd3ef49564e76d2ecfe2678a7b0bc078f4
Roman <Dadlord36@gmail.com>
2020-07-16 19:39:47 +0300

    BuildScript is now filling app-deployment passwords

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -7,9 +7,13 @@ public static class BuildScript
     private static readonly string[] DefaultScene = {"Assets/Scenes/Main.unity"};
     private const string AndroidExtension = ".apk";
 
+    private const string KeyStorePassword = "TtZWc6#7TK@r";
+
+
     [MenuItem("Builds/Build For Android_Mono_Development")]
     public static void BuildForAndroid_Mono_Development()
     {
+        SetKeyStorePasswords();
         PerformBuild(BuildTarget.Android, BuildTargetGroup.Android, ScriptingImplementation.Mono2x, AndroidArchitecture.ARMv7,
             BuildOptions.Development, $"{AppName}_Development_ARMv7{AndroidExtension}");
     }
@@ -17,8 +21,15 @@ public static class BuildScript
     [MenuItem("Builds/Build For Android_IL2CPP_Release")]
     public static void BuildForAndroid_IL2CPP_Release()
     {
+        SetKeyStorePasswords();
         PerformBuild(BuildTarget.Android, BuildTargetGroup.Android, ScriptingImplementation.IL2CPP, AndroidArchitecture.ARM64, BuildOptions.None,
             $"{AppName}_Release_IL2CPP_ARM64{AndroidExtension}");
+    }
+
+    private static void SetKeyStorePasswords()
+    {
+        PlayerSettings.keystorePass = KeyStorePassword;
+        PlayerSettings.keyaliasPass = KeyStorePassword;
     }
 
     private static void PerformBuild(BuildTarget buildTarget, BuildTargetGroup buildTargetGroup, ScriptingImplementation scriptingImplementation,
