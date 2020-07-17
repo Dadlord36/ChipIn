bab656f37b45ca9324ec88d6fd257641ee750414
Roman <Dadlord36@gmail.com>
2020-07-17 18:52:11 +0300

    Connect view model implemented.

---
+++ Assets/Scripts/Chip-In/ViewModels/ConnectViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityWeld.Binding;
+
+namespace ViewModels
+{
+    [Binding]
+    public sealed class ConnectViewModel : ViewsSwitchingViewModel, INotifyPropertyChanged
+    {
+        private bool _listIsNotEmpty;
+
+        [Binding]
+        public bool ListIsNotEmpty
+        {
+            get => _listIsNotEmpty;
+            private set
+            {
+                if (value == _listIsNotEmpty) return;
+                _listIsNotEmpty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ConnectViewModel() : base(nameof(ConnectViewModel))
+        {
+        }
+
+        [Binding]
+        public void CreateAdButton_OnClick()
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
