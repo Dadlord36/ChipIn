e0e68b1ff0d4dfdfa08c54615bc2949a334ee6f4
Roman <dadlord36@gmail.com>
2020-07-28 11:38:30 +0300

    Redemption form update.

--- Assets/Scripts/Chip-In/ViewModels/QrCodeScannerViewModel.cs
+++ Assets/Scripts/Chip-In/ViewModels/QrCodeScannerViewModel.cs
@@ -1,38 +1,72 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using HttpRequests.RequestsProcessors.PutRequests;
+using JetBrains.Annotations;
 using Repositories.Remote;
 using RequestsStaticProcessors;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityWeld.Binding;
 using Utilities;
 using Views;
 using Object = UnityEngine.Object;
+using RectTransformUtility = Utilities.RectTransformUtility;
 
 namespace ViewModels
 {
     [Binding]
-    public class QrCodeScannerViewModel : ViewsSwitchingViewModel
+    public sealed class QrCodeScannerViewModel : ViewsSwitchingViewModel, INotifyPropertyChanged
     {
         [SerializeField] private UserAuthorisationDataRepository authorisationDataRepository;
         [SerializeField] private Object qrPreviewPrefab;
         [SerializeField] private CodeReader qrCodeReader;
         [SerializeField] private PreviewController qrPreviewController;
+        [SerializeField] private Transform cameraViewContainer;
 
-        private float _restartTime;
-        private bool _isInitialized;
+        public UnityEvent scanningHasFailed;
 
         private GameObject _qrPreviewRoot;
+        private bool _isBusy;
+
+        [Binding]
+        public bool IsBusy
+        {
+            get => _isBusy;
+            private set
+            {
+                if (value == _isBusy) return;
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
 
         public QrCodeScannerViewModel() : base(nameof(QrCodeScannerViewModel))
         {
         }
 
         [Binding]
-        public void ScanButton_OnClick()
+        public async void ScanButton_OnClick()
         {
-            SwitchToView(nameof(RedeemedView));
+            try
+            {
+                IsBusy = true;
+                var result = await qrCodeReader.DecodeQRAsync().ConfigureAwait(true);
+                if (result!= null && !string.IsNullOrEmpty(result.Text))
+                {
+                    ProcessDecodedString(result.Text);
+                }
+                else
+                {
+                    OnScanningHasFailed();
+                }
+            }
+            catch (Exception e)
+            {
+                LogUtility.PrintLogException(e);
+            }
         }
 
         protected override void OnBecomingActiveView()
@@ -41,11 +75,32 @@ namespace ViewModels
             TryAuthorizeWebCameraAndStartQrReader();
         }
 
+
         protected override void OnBecomingInactiveView()
         {
             base.OnBecomingInactiveView();
-            DeactivateQrScanning();
+            qrPreviewController.StopWork();
             DestroyPreviewObject();
+        }
+
+        private async void ProcessDecodedString(string decodedString)
+        {
+            Handheld.Vibrate();
+            try
+            {
+                IsBusy = true;
+                await ActivateProductAsync(decodedString).ConfigureAwait(false);
+                SwitchToView(nameof(RedeemedView));
+            }
+            catch (Exception e)
+            {
+                LogUtility.PrintLogException(e);
+                throw;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         private void CreatePreviewObject()
@@ -62,37 +117,17 @@ namespace ViewModels
         private void Initialize()
         {
             qrPreviewController.rawimg = _qrPreviewRoot.GetComponentInChildren<RawImage>();
-            if (_isInitialized) return;
+            _qrPreviewRoot.transform.SetParent(cameraViewContainer);
+            qrCodeReader.previewctr = qrPreviewController;
             qrCodeReader.Initialize();
-            _isInitialized = true;
+            RectTransformUtility.Centralize(_qrPreviewRoot.transform as RectTransform);
+            qrPreviewController.StartWork();
         }
 
         private void ActivateQrScanning()
         {
             CreatePreviewObject();
             Initialize();
-            CodeReader.OnCodeFinished += CodeReaderOnCodeFinished;
-            qrCodeReader.StartWork();
-        }
-
-        private void DeactivateQrScanning()
-        {
-            CodeReader.OnCodeFinished -= CodeReaderOnCodeFinished;
-            qrCodeReader.StopWork();
-        }
-
-        private async void CodeReaderOnCodeFinished(string decodedText)
-        {
-            Handheld.Vibrate();
-            try
-            {
-                await ActivateProductAsync(decodedText).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                LogUtility.PrintLogException(e);
-                throw;
-            }
         }
 
         private Task ActivateProductAsync(string decodedText)
@@ -120,6 +155,20 @@ namespace ViewModels
 
                 ActivateQrScanning();
             };
+        }
+
+
+        private void OnScanningHasFailed()
+        {
+            scanningHasFailed.Invoke();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
