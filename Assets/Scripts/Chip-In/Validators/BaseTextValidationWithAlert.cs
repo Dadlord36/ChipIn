03a57de5d1b174937d79f247d78b00c10dae8e3d
Roman <dadlord36@gmail.com>
2020-08-04 17:13:04 +0300

    Registration form validation updated. Ref #89

--- Assets/Scripts/Chip-In/Validators/BaseTextValidationWithAlert.cs
+++ Assets/Scripts/Chip-In/Validators/BaseTextValidationWithAlert.cs
@@ -13,64 +13,20 @@ namespace Validators
         bool IsValid { get; }
     }
 
+
     [Binding]
-    public abstract class BaseTextValidationWithAlert<T> : MonoBehaviour, INotifyPropertyChanged, IValidationWithAlert
+    public abstract class BaseTextValidationWithAlert<T> : BaseValidationWithAlert
     {
         [SerializeField] private TextValidation validation;
 
         [Binding] public T TextToValidate { get; set; }
 
         private object PropertyToValidate => TextToValidate;
-        private bool _isValid = true;
-        private bool _showAlert;
-
-        [Binding]
-        public bool IsValid
-        {
-            get => _isValid;
-            private set
-            {
-                if (value == _isValid) return;
-                _isValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-        [Binding]
-        public bool ShowAlert
-        {
-            get => _showAlert;
-            set
-            {
-                if (value == _showAlert) return;
-                _showAlert = value;
-                OnPropertyChanged();
-            }
-        }
 
 
-        [Binding]
-        public void HideAlertText()
-        {
-            ShowAlert = false;
-        }
-        
-        private bool CheckIsValid()
+        protected override bool CheckIsValid()
         {
             return IsValid = validation.CheckIsValid(PropertyToValidate);
-        }
-
-        public void ShowAlertIfIsNotValid()
-        {
-            ShowAlert = !CheckIsValid();
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
