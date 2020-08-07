e6a73bd3e5df712de495344cbea0f13be808081f
Roman <dadlord36@gmail.com>
2020-08-07 16:08:44 +0300

    TasksFactories and default icon

---
+++ Assets/Scripts/Chip-In/Tasking/TasksFactories.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasking
+{
+    public static class TasksFactories
+    {
+        public static readonly TaskFactory MainThreadTaskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.DenyChildAttach,
+            TaskContinuationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+    }
+}
