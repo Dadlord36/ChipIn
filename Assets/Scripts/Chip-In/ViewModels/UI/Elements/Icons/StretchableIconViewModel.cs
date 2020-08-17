ef48328dcc077d963e3840279ffde4ef78281ae2
Roman <Dadlord36@gmail.com>
2020-08-17 15:47:14 +0300

    StretchableIconViewModel implemented.

---
+++ Assets/Scripts/Chip-In/ViewModels/UI/Elements/Icons/StretchableIconViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Controllers;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityWeld.Binding;
+
+namespace ViewModels.UI.Elements.Icons
+{
+    [Binding]
+    [RequireComponent(typeof(IconSizeFitterController))]
+    public sealed class StretchableIconViewModel : Image
+    {
+        public override void SetAllDirty()
+        {
+            GetComponent<IconSizeFitterController>().FitImage();
+            base.SetAllDirty();
+        }
+    }
+}
