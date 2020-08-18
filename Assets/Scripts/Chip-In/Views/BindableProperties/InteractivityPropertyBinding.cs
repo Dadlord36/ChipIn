2a36e6f880362fec5aee65572b7622114ab30c5f
Roman <Dadlord36@gmail.com>
2020-08-18 15:53:45 +0300

    User Bottom Bar now has limited interactivity. Only Market, Settings and Cart are active.

---
+++ Assets/Scripts/Chip-In/Views/BindableProperties/InteractivityPropertyBinding.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityWeld.Binding;
+
+namespace Views.BindableProperties
+{
+    [Binding]
+    public sealed class InteractivityPropertyBinding : MonoBehaviour, INotifyPropertyChanged
+    {
+        [SerializeField] private bool isInteractable;
+
+        [Binding]
+        public bool IsInteractable
+        {
+            get => isInteractable;
+            set
+            {
+                if (value == isInteractable) return;
+                isInteractable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
