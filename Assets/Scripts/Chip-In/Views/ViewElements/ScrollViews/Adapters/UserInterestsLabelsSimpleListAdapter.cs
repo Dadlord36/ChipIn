281f1c3fcf6efede174255bcbe4317a1f4847ea3
Roman <Dadlord36@gmail.com>
2020-08-25 18:32:16 +0300

    UserInterestsLabelsSimpleAdapter implemented.

---
+++ Assets/Scripts/Chip-In/Views/ViewElements/ScrollViews/Adapters/UserInterestsLabelsSimpleListAdapter.cs
@@ -0,0 +1,23 @@
+﻿using Com.TheFallenGames.OSA.CustomParams;
+using Common;
+using DataModels;
+using ViewModels.Cards;
+using Views.ViewElements.ScrollViews.Adapters.BaseAdapters;
+using Views.ViewElements.ScrollViews.Adapters.ViewFillingAdapters;
+using Views.ViewElements.ScrollViews.ViewHolders;
+
+namespace Views.ViewElements.ScrollViews.Adapters
+{
+    public class UserInterestsLabelsSimpleListAdapter : BasedListAdapter<BaseParamsWithPrefab, DefaultFillingViewPageViewHolder<InterestItemViewModel.FieldFillingData>,
+        InterestBasicDataModel, InterestItemViewModel.FieldFillingData, UserInterestsLabelsSimpleListAdapter.UserInterestsLabelsFillingViewAdapter>
+    {
+        public class UserInterestsLabelsFillingViewAdapter : FillingViewAdapter<InterestBasicDataModel, InterestItemViewModel.FieldFillingData>
+        {
+            public override InterestItemViewModel.FieldFillingData Convert(DisposableCancellationTokenSource cancellationTokenSource, InterestBasicDataModel data,
+                uint dataIndexInRepository)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
