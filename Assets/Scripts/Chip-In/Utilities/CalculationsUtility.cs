8c241f6ae6b591ee9f503472c337a6826f385679
Roman <Dadlord36@gmail.com>
2020-09-14 15:38:00 +0300

    CalculationsUtility.cs added.

---
+++ Assets/Scripts/Chip-In/Utilities/CalculationsUtility.cs
@@ -0,0 +1,14 @@
+﻿namespace Utilities
+{
+    public static class CalculationsUtility
+    {
+        public static int GetMiddle(int number)
+        {
+            // if length is odd, return middle numer
+            if (number % 2 == 1) return number/2;
+
+            // if length is even return the first of the two middle numbers
+            return number/2  - 1;
+        }
+    }
+}
