3f2f1841758aa29de64a4a7b2dfadcdf455425d1
Roman <Dadlord36@gmail.com>
2020-09-16 19:16:14 +0300

    TasksFactories.cs updated.

--- Assets/Scripts/Chip-In/Tasking/TasksFactories.cs
+++ Assets/Scripts/Chip-In/Tasking/TasksFactories.cs
@@ -14,9 +14,24 @@ namespace Tasking
             MainThreadTaskFactory.StartNew(action).GetAwaiter().GetResult();
         }
 
+        public static Task<T> ExecuteOnMainThreadTaskAsync<T>(Func<T> func)
+        {
+            return MainThreadTaskFactory.StartNew(func);
+        }
+
         public static T ExecuteOnMainThread<T>(Func<T> func)
         {
             return MainThreadTaskFactory.StartNew(func).GetAwaiter().GetResult();
+        }
+        
+        public static async Task PeriodicAsync(Func<Task> taskFactory, TimeSpan interval, CancellationToken cancellationToken = default)
+        {
+            while (true)
+            {
+                var delayTask = Task.Delay(interval, cancellationToken);
+                await taskFactory().ConfigureAwait(false);
+                await delayTask.ConfigureAwait(false);
+            }
         }
     }
 }
